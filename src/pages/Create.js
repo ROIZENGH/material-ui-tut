c480cc43e0d073a132859550c19cb43519411495
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-09-27 12:10:34 -0500

    lesson 6

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Typography, Button, Container } from "@mui/material";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 export default function Create() {
-  return (
-    <div>
-      Create page
-    </div>
-  )
+	return (
+		<Container>
+			<Typography
+				variant="h6"
+				color="textSecondary"
+				component="h2"
+				gutterBottom
+			>
+				Create a New Note
+			</Typography>
+			<Button
+				type="submit"
+				color="secondary"
+				variant="contained"
+				onClick={() => console.log("you clicked me!")}
+				endIcon={<KeyboardArrowRightIcon />}
+			>
+				Submit
+			</Button>
+		</Container>
+	);
 }
